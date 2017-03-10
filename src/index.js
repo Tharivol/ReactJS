db85eb1fcdc7916e8dc1c6ad51d410f6cc80fee7
Tharivol <hughes.brewzz@gmail.com>
2017-03-09 21:48:07 -0700

    gitignore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyDK1XxWWrQ4cG8LjhMLISnhfFlsSFLANRs';
 
 
@@ -22,6 +24,7 @@ class App extends Component	{
 		return (
 		<div>
 		<SearchBar />
+		<VideoDetail viedo={this.state.videos[0]} />
 		<VideoList videos={this.state.videos}/>
 	</div>
 	);
